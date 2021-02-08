f07f021042aae5c053f6371721e90d9e4051e7fa
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-02-09 02:56:07 +0530

    Added Point & Dimension class

--- constants.py
+++ constants.py
@@ -1,3 +1,23 @@
 MAXLIFE = 3
 FRAMEWIDTH = 150
-FRAMEHEIGHT = 10
+FRAMEHEIGHT = 45
+PADDLEWIDTH = 20
+PADDLEHEIGHT = 1
+
+
+class Point:
+    '''
+    Defines a co-ordinate
+    '''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Dimension:
+    '''
+    Defines dimensions of an object in 2D world
+    '''
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
