c452892a24bea0800efad03267ae828210593f80
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-02-09 00:46:19 +0530

    Added sample manger for the the current game

--- manager.py
+++ manager.py
@@ -1,12 +1,17 @@
+from frame import Frame
+
 class Manager:
-    def __init__(self, GS):
+    def __init__(self, game_status):
         '''
         construct the walls, paddle, ball and frame
         '''
+        self.game_status = game_status
+        self.frame = Frame(self.game_status)
+        self.frame.display()
         while True:
             self.schedular()
     
     def schedular(self):
         '''
         Order of execution at each iteration.
-        '''
+        ''' 
