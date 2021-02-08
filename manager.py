d19894bdeb791de4e894b0634ce055801b39b7e6
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-02-08 19:32:38 +0530

    Temporary Game Manager

---
+++ manager.py
@@ -0,0 +1,12 @@
+class Manager:
+    def __init__(self, GS):
+        '''
+        construct the walls, paddle, ball and frame
+        '''
+        while True:
+            self.schedular()
+    
+    def schedular(self):
+        '''
+        Order of execution at each iteration.
+        '''
