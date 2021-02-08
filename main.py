4852bebbbba5073f4c31a30da64e58e1952a86e1
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-02-08 19:31:32 +0530

    Added status of the game file

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 '''
 Game Execution will start from here!
 '''
-from os import system
 from status import Status
 from colorama import init,Fore,Back,Style
 
-init()
-Game = Status()
-while(1):
-    system('clear')
-    Game.print_status()
+if __name__ == '__main__':
+    init()
+    Game = Status()
+    Game.start_game()
