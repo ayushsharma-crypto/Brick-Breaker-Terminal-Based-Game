2af2b237beb7c60e5ef36234b1a2f3e32bd20aea
Ayush Sharma <5460563-ayushsharma-crypto@users.noreply.gitlab.com>
2021-03-17 14:13:45 +0530

    Next task is to implement power ups.

---
+++ v2/powerup.py
@@ -0,0 +1,10 @@
+from frame import Frame
+from ball import Ball
+
+
+class PowerUp:
+    '''
+    A class for various power implementation.
+    '''
+    def __init__(self,ball: Ball,frame: Frame):
+        pass
